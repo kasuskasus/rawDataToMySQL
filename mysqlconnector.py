ca1d1591b87679046f804061e620d84d8c83c3f9
Home <gn@arbor.capital>
2018-01-16 22:01:51 +0300

    Database initialization working

--- mysqlconnector.py
+++ mysqlconnector.py
@@ -6,6 +6,11 @@ import mysql.connector
 import config # project config file
 import markets
 import logging
+from sys import stdout
+
+def std_write(n, m):
+    stdout.write("\r Element %d" % n + " out of %d" % m)
+    stdout.flush()
 
 class MySqlExchangeProcessor():
     """ Used to establish connection to MySql database, prepare clean table structure based on markets data
@@ -29,7 +34,9 @@ class MySqlExchangeProcessor():
             print("Error %d: %s" % (e.args[0], e.args[1]))
             sys.exit(1)
 
-        if not database_ready:
+        if database_ready:
+            print("Database successfully initialized!")
+        else:
             raise RuntimeError
 
     def prepare_database(self):
@@ -55,19 +62,51 @@ class MySqlExchangeProcessor():
         _sql_tables_create_stmt = []
         for each in markets.list_all_markets():
             _sql_tables_create_stmt.append("CREATE TABLE %s_sell_orders LIKE _sell_order_template" % each)
-            # _sql_tables_create_stmt += "CREATE TABLE %s_buy_orders LIKE __buy_order_template;\n" % each
-            # _sql_tables_create_stmt += "CREATE TABLE %s_trades LIKE _trade_template;\n" % each
+            _sql_tables_create_stmt.append("CREATE TABLE %s_buy_orders LIKE _buy_order_template" % each)
+            _sql_tables_create_stmt.append("CREATE TABLE %s_trades LIKE _trade_template" % each)
         _cursor = self._connection.cursor()
         print('Creating new tables for all markets from templates...')
         try:
+            n = 0
+            m = len(_sql_tables_create_stmt)
             for each in _sql_tables_create_stmt:
                 _cursor.execute(each)
+                n += 1
+                std_write(n, m)
+            print("")
             _cursor.close()
         except mysql.connector.Error as e:
             print(e)
 
-        # Database initialized
-        return True
+        # Database exists?
+        databases_existing = []
+        for x in self.get_databases():
+            databases_existing.append(x[0])
+
+        database_exists = self._database in databases_existing
+
+        # Tables exist?
+        tables_existing = []
+        a = ""
+        for x in self.get_tables(self._database):
+            a = x[0]
+            a = a.replace("_sell_orders","")
+            a = a.replace("_buy_orders","")
+            a = a.replace("_trades","")
+            if a != "_sell_order_template" and a != "_buy_order_template" and a != "_trade_template":
+                tables_existing.append(a)
+
+        # Getting rid of duplicates
+        tables_existing=list(dict.fromkeys(tables_existing))
+
+        # Comparing 2 lists - if zero len -> list are identical
+        if len(set(tables_existing) ^ set(markets.list_all_markets())) == 0:
+            tables_exist = True
+
+        if database_exists and tables_existing:
+            return True
+        else:
+            return False
 
     def close(self):
         self._connection.close()
@@ -92,19 +131,6 @@ class MySqlExchangeProcessor():
         _cursor.close()
         return _results
 
-    def remove_tables_from_db(self, database_name=None, list_of_tables = None):
-        if not database_name:
-            database_name = self._database
-
-        _tables = show_tables(database_name)
-
-        if _tables and list_of_tables:
-            # self._cursor.execute("USE %s" & database_name)
-            _q = "DROP TABLE IF EXISTS "
-            print(_q)
-
-
-
 if __name__ == "__main__":
     mysql = MySqlExchangeProcessor(
                                     user=config.config['db_user'],
@@ -112,9 +138,6 @@ if __name__ == "__main__":
                                     host=config.config['db_host'],
                                     database=config.config['db_name']
     )
-
-
-
 
     mysql.close()
 
